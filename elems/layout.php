cf0ed8b05b47a7fbe913fa085ce730fd5885cb6d
KirillGul <50946729+KirillGul@users.noreply.github.com>
2021-06-01 23:16:29 +0300

    SVG изм

--- elems/layout.php
+++ elems/layout.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php if(!empty($meta)) echo $meta; //для категорий?>
     <?php if(!empty($meta2)) echo $meta2; //для категорий?>
-    <link rel="icon" href="<?php echo $prefhostHTTP.$hostHTTP; ?>/assets/favicon.svg" type=" image/svg+xml"> 
+    <link rel="icon" href="<?php echo $prefhostHTTP.$hostHTTP; ?>/assets/favicon.svg" type=" image/svg+xml">
     <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
     <?php if(!empty($ReadMoreJS)) echo $ReadMoreJS; //для товара?> 
     <script src="/assets/scripts.js" async="async"></script>    
